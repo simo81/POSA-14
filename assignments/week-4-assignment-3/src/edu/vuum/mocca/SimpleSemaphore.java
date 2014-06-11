aaeb3f34077d602238c651ac1e689472c70a5f46
Kamil Seweryn <sewerk@gmail.com>
2014-06-11 16:19:23 +0200

    assignment week 4

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,6 +1,8 @@
 package edu.vuum.mocca;
-import java.util.concurrent.locks.Condition;
+
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
 
 /**
  * @class SimpleSemaphore
@@ -15,22 +17,28 @@ public class SimpleSemaphore {
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+	private Lock lock;
 
     /**
      * Define a Condition that waits while the number of permits is 0.
      */
     // TODO - you fill in here
+	private Condition condition;
 
     /**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here. Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+	private volatile int permits;
 
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+    	this.lock  = new ReentrantLock(fair);
+    	this.condition = lock.newCondition();
+    	this.permits = permits;
     }
 
     /**
@@ -39,6 +47,13 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+    	lock.lockInterruptibly();
+    	while (permits == 0) {
+    		condition.await();
+    	}
+    	--permits;
+    	
+    	lock.unlock();
     }
 
     /**
@@ -47,6 +62,13 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+    	lock.lock();
+    	while (permits == 0) {
+    		condition.awaitUninterruptibly();
+    	}
+    	--permits;
+    	
+    	lock.unlock();
     }
 
     /**
@@ -54,13 +76,18 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+    	lock.lock();
+    	++permits;
+    	condition.signalAll();
+    	lock.unlock();
     }
 
     /**
      * Return the number of permits available.
      */
     public int availablePermits() {
-        // TODO - you fill in here to return the correct result
-    	return 0;
+        // TODO - you fill in here by changing null to the appropriate
+        // return value.
+        return permits;
     }
 }
